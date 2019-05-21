df8c5c097a96cdabdfe2d04d5029c7503f7e8265
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-05-21 18:17:51 -0500

    implemented navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
+import NavBar from "./components/NavBar.js"
 // RHL only for front end development
 //import { hot } from 'react-hot-loader';
+
 
 class App extends Component {
   render() {
     return (
         <div>
-         <form>
-         <label>
-         Name:
-         <input type="text" name="name" />
-         </label>
-         <input type="submit" value="Submit" />
-         </form></div>
+          <NavBar />
+        </div>
     );
   }
 }
---
+++ client/src/dummy_data.js
@@ -0,0 +1,12 @@
+
+const data = [
+  {
+    name:"Wooca",
+    Breed:"french bulldog",
+    Age:"2 years",
+    Pedigree:"no",
+    City:"CDMX"
+  }
+];
+
+export default data;
