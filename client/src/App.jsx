9df3b885f3f0a0376c34af4546c48aa9720803e5
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-05-29 19:40:29 -0500

    started implementing react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import NavBar from "./components/NavBar.jsx"
+import React, { Component, Fragment } from 'react';
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
 import LogIn from "./components/LogIn.jsx"
-import Profile from "./components/Profile.jsx"
 import Matches from "./components/MainCard.jsx"
+import Profile from "./components/Profile.jsx"
 // RHL only for front end development
 //import { hot } from 'react-hot-loader';
 
@@ -10,12 +12,31 @@ import Matches from "./components/MainCard.jsx"
 class App extends Component {
   render() {
     return (
-        <div>
-          <NavBar />
-          <LogIn />
-          <Profile />
-          <Matches />
-        </div>
+      <div>>
+        <LogIn />
+      
+      <BrowserRouter>
+      <div className="App">
+        <Route
+          path="/"
+          render={({ location }) => (
+            <Fragment>
+              <Tabs value={location.pathname}>
+                <Tab label="Profile" component={Link} to="/components/Profile.jsx" />
+                <Tab label="Matches" component={Link} to="/components/MainCard.jsx" />
+                />
+              </Tabs>
+              <Switch>
+                <Route path="/components/MainCard.jsx" render={() => <div>Profile</div>}
+                />
+                <Route path="/components/Profile.jsx" render={() => <div>Matches</div>} />
+              </Switch>
+            </Fragment>
+          )}
+        />
+      </div>
+    </BrowserRouter>
+    </div>
     );
   }
 }
